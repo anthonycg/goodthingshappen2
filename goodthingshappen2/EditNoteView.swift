11c963a84d5ca43132d8681b65d5a5b55452fc9d
anthonycg <anthonygibson24@gmail.com>
2024-11-05 00:35:55 -0600

    edit note screen placeholder text update

--- goodthingshappen2/EditNoteView.swift
+++ goodthingshappen2/EditNoteView.swift
@@ -11,40 +11,65 @@ import FirebaseAuth
 
 struct EditNoteView: View {
     @Bindable var note: Note6
+    
     var body: some View {
         ZStack {
             Color(.champagnePink).ignoresSafeArea()
             
             VStack {
-//                @FocusState var emailFieldIsFocused: Bool = false
-                
-                TextField("Title your day...", text: $note.postTitle, axis: .vertical)
-                    .scrollContentBackground(.hidden)
-                    .foregroundColor(.black).textInputAutocapitalization(.sentences)
-                    .font(.custom("HelveticaNeue", size: 48)).frame(maxHeight: 200, alignment: .topLeading).padding()
+                ZStack(alignment: .topLeading) {
+                    if note.postTitle.isEmpty {
+                        Text("Title your day...")
+                            .foregroundColor(.black) // Set placeholder text color
+                            .font(.custom("HelveticaNeue", size: 48))
+                            .padding(.horizontal)
+                            .padding(.top, 8)
+                    }
+                    
+                    TextField("", text: $note.postTitle, axis: .vertical)
+                        .scrollContentBackground(.hidden)
+                        .foregroundColor(.black).opacity(0.3)
+                        .textInputAutocapitalization(.sentences)
+                        .font(.custom("HelveticaNeue", size: 48))
+                        .frame(maxHeight: 200, alignment: .topLeading)
+                        .padding()
+                }
                 
                 if let imageData = note.imageURL,
                    let uiImage = UIImage(data: imageData) {
                     Image(uiImage: uiImage)
                         .resizable()
-                        .scaledToFill()  // Use scaledToFill to cover the area
-                        .frame(maxWidth: .infinity, minHeight: 225, maxHeight: 225) // Match the rectangle size
+                        .scaledToFill()
+                        .frame(maxWidth: .infinity, minHeight: 225, maxHeight: 225)
                         .clipped()
                         .cornerRadius(30)
                         .padding(.bottom)
                 }
+                
                 Spacer()
                 
-                TextField("What good thing happened today?", text: $note.postBody, axis: .vertical)
-                    .scrollContentBackground(.hidden)
-                    .foregroundColor(.black).textInputAutocapitalization(.sentences)
-                    .font(.custom("HelveticaNeue", size: 20)).frame(maxHeight: .infinity, alignment: .topLeading).padding(.horizontal)
-                
+                ZStack(alignment: .topLeading) {
+                    if note.postBody.isEmpty {
+                        Text("What good thing happened today?")
+                            .foregroundColor(.black).opacity(0.3) // Set placeholder text color
+                            .font(.custom("HelveticaNeue", size: 20))
+                            .padding(.horizontal)
+                            .padding(.top, 8)
+                    }
+                    
+                    TextField("", text: $note.postBody, axis: .vertical)
+                        .scrollContentBackground(.hidden)
+                        .foregroundColor(.black)
+                        .textInputAutocapitalization(.sentences)
+                        .font(.custom("HelveticaNeue", size: 20))
+                        .frame(maxHeight: .infinity, alignment: .topLeading)
+                        .padding(.horizontal)
+                }
             }
-            
         }
     }
 }
+
 
 #Preview {
     // Create an example note
